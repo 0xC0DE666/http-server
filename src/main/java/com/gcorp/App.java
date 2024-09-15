82a865e79537120877b17549bab86b9bd8a4edc4
0xC0DE666 <0xC0DE666@proton.me>
2024-09-15 12:03:48 +0200

    listening for single connection

--- src/main/java/com/gcorp/App.java
+++ src/main/java/com/gcorp/App.java
@@ -1,5 +1,11 @@
 package com.gcorp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import com.gcorp.config.ConfigManager;
 
 /**
@@ -7,9 +13,58 @@ import com.gcorp.config.ConfigManager;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Server starting...");
         ConfigManager.getInstance().loadConfig("src/main/resources/config.json");
+
         var conf = ConfigManager.getInstance().getConfig();
-        System.out.println(conf.getPort() + " " + conf.getRoot());
+
+        System.out.println("Server listening...");
+        System.out.println("port: " + conf.getPort());
+        System.out.println("web root: " + conf.getRoot());
+
+        try {
+          ServerSocket serverSocket = new ServerSocket(conf.getPort());
+          Socket clientSocket = serverSocket.accept();
+
+          InputStream in = clientSocket.getInputStream();
+          OutputStream out = clientSocket.getOutputStream();
+
+          String res = httpResponse(landing());
+          out.write(res.getBytes());
+
+          in.close();
+          out.close();
+          clientSocket.close();
+          serverSocket.close();
+
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+    }
+
+    private static String httpResponse(String html) {
+      StringBuilder buffer = new StringBuilder();
+      final String CRLF = "\n\r"; // 13, 10 ASCII
+
+      buffer.append("HTTP/1.1 200 OK");
+      buffer.append(CRLF);
+      buffer.append("Content-Length: " + html.getBytes().length + CRLF + CRLF);
+      buffer.append(html +  CRLF + CRLF);
+
+      return buffer.toString();
+    }
+
+    private static String landing() {
+      String html = """
+        <html>
+          <head>
+            <title>Web server from scratch</title>
+          </head>
+          <body>
+            <h1>Http Web Server</h1>
+            <p>This page is being served by an HTTP server, written from scratch using TCP sockets in java. :)</p>
+          </body>
+        </html>
+      """;
+      return html;
     }
 }
