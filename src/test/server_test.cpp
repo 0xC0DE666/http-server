095cae3e42e0fcac66e8089d2c041637472a9af3
0xC0DE666 <0xC0DE666@proton.me>
2024-10-26 16:25:16 +0200

    test both config load functions

--- src/test/server_test.cpp
+++ src/test/server_test.cpp
@@ -1,14 +1,28 @@
+#include <cstdlib>
+
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 
 #include "../app/server.hpp"
 
 // ####################
-// load_config
+// Config_load
 // ####################
-Test(load_config, _1) {
-  Config expected = {8080, ""};
+Test(Config_load, from_env) {
+  Config expected = {8080, "/srv/www"};
   Config* actual = Config::load();
+  cr_assert_eq(actual->PORT, expected.PORT);
+  cr_assert_eq(actual->WWW_DIR, expected.WWW_DIR);
+
+  delete actual;
+}
+
+Test(Config_load, from_file) {
+  using namespace std;
+
+  string cwd = string(getenv("PWD"));
+  Config expected = {8080, "/srv/www"};
+  Config* actual = Config::load(cwd + "/src/assets/config.json");
   cr_assert_eq(actual->PORT, expected.PORT);
   cr_assert_eq(actual->WWW_DIR, expected.WWW_DIR);
 
